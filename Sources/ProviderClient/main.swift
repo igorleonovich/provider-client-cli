8f12ba9b4ca1ee1091528994ee1b0d0fa90723ec
name <email>
2020-02-06 23:51:15 +0300

    no message

--- Sources/ProviderClient/main.swift
+++ Sources/ProviderClient/main.swift
@@ -1,1 +1,35 @@
-print("Hello, world!")
+import Foundation
+import Starscream
+
+class WSClient: WebSocketDelegate {
+    
+    var socket: WebSocket!
+    var isConnected = false
+    
+    init() {
+        socket = WebSocket(url: URL(string: "ws://localhost:8888/echo")!)
+        socket.delegate = self
+        socket.connect()
+    }
+    
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("connected")
+        socket.write(string: "⛳️")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("disconnected")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        print("received: \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        
+    }
+}
+
+let wsc = WSClient()
+print("...")
+RunLoop.main.run()
